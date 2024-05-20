5876ad2f7403e8578c512fa883cda35fc0612aad
Clément Poulin <clement.poulin.1@ulaval.ca>
2024-05-20 00:59:48 -0400

    MLP v3 start

---
+++ ChatGPT/Makemore/MLP/MLP v3.py
@@ -0,0 +1,188 @@
+import torch as t
+import math as m
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import random
+import matplotlib.gridspec as gridspec
+
+from functions import *
+from datasets import *
+
+
+# get data
+input_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'datasets', 'names.txt')
+words = open(input_file_path, 'r').read().splitlines()
+
+# lookup tables for s --> i || i --> s
+chars = sorted(list(set(''.join(words))))
+stoi = {s: i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i: s for s,i in stoi.items()}
+vocab_size = len(itos)
+
+# build the dataset
+block_size = 3
+
+
+class Linear:
+
+    def __init__(self, in_features, out_features, bias=True):
+        self.weight = t.randn((in_features, out_features), generator=g) / m.sqrt(in_features)
+        self.bias = t.zeros(out_features) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])
+
+
+class BatchNorm1d:
+
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+
+        # parameters (trained with backprop)
+        self.gamma = t.ones(dim)
+        self.beta = t.zeros(dim)
+
+        # buffers (*trained with a running "momentum update")
+        self.running_mean = t.zeros(dim)
+        self.running_var = t.ones(dim)
+
+    def __call__(self, x):
+
+        # calculate the forward pass
+        if self.training:
+            xmean = x.mean(0, keepdim=True)                 # batch mean
+            xvar = x.var(0, keepdim=True, unbiased=True)    # batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+
+        xhat = (x - xmean) / t.sqrt(xvar + self.eps)    # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+
+        # update the buffers
+        if self.training:
+            with t.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+
+class Tanh:
+
+    def __call__(self, x):
+        self.out = t.tanh(x)
+        return self.out
+
+    @staticmethod
+    def parameters():
+        return []
+
+
+random.seed(1789)
+random.shuffle(words)
+
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+
+# create splits
+Xtr,  Ytr  = build_dataset(words[:n1], stoi=stoi, itos=itos, block_size=block_size)      # 80%
+Xdev, Ydev = build_dataset(words[n1:n2], stoi=stoi, itos=itos, block_size=block_size)    # 10%
+Xte,  Yte  = build_dataset(words[n2:], stoi=stoi, itos=itos, block_size=block_size)      # 10%
+
+# model initialization
+n_embd = 10       # dimensionality of the character embedding vectors
+n_hidden = 100    # number of neurons in the hidden layer
+g = t.Generator().manual_seed(1789)
+
+C = t.randn((vocab_size, n_embd))
+
+layers = [
+    Linear(n_embd * block_size, n_hidden), Tanh(),
+    Linear(n_hidden,            n_hidden), Tanh(),
+    Linear(n_hidden,            n_hidden), Tanh(),
+    Linear(n_hidden,            n_hidden), Tanh(),
+    Linear(n_hidden,            n_hidden), Tanh(),
+    Linear(n_hidden,          vocab_size),
+]
+
+with t.no_grad():
+    # last layer : make less confident
+    layers[-1].weight *= 0.1
+    # all other layers : apply gain
+    for layer in layers[:-1]:
+        if isinstance(layer, Linear):
+            layer.weight *= 5/3
+
+parameters = [C] + [p for layer in layers for p in layer.parameters()]
+
+# total number of parameters
+print(sum(p.nelement() for p in parameters))
+
+for p in parameters:
+    p.requires_grad = True
+
+# optimization
+max_steps = 200000
+batch_size = 32
+lossi = []
+
+for i in range(max_steps):
+
+    # minibatch construct
+    ix = t.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+    Xb, Yb = Xtr[ix], Ytr[ix]    # batch X,Y
+
+    # forward pass
+    emb = C[Xb]                      # embeds the characters into vectors
+    x = emb.view(emb.shape[0], -1)    # concatenate the vectors
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, Yb)
+
+    # backward pass
+    for layer in layers:
+        layer.out.retain_grad()
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+
+    # update
+    lr = 0.1 if i < max_steps/2 else 0.01    # step learning rate decay
+    for p in parameters:
+        p.data += -lr * p.grad
+
+    # track stats
+    if i % max_steps/10 == 0:
+        print(f"{i:7d}/{max_steps:7d}: {loss.item():.4f}")
+    lossi.append(loss.log10().item())
+
+    break
+
+# visualize stats
+plt.figure(figsize=(19, 4))
+legends = []
+for i, layer in enumerate(layers[:-1]):    # excludes the output layer
+    if isinstance(layer, Tanh):
+        tt = layer.out
+        print(f"layer %d (%{len(layer.__class__.__name__)}s): mean %+.2f, std %.2f, saturated: %.2f%%" % (i, layer.__class__.__name__, tt.mean(), tt.std(), (tt.abs() > 0.97).float().mean()*100))
+        hy, hx = t.histogram(tt, density=True)
+        plt.plot(hx[:-1].detach(), hy.detach())
+        legends.append(f"layer {i} ({layer.__class__.__name__})")
+
+plt.legend(legends)
+plt.title("Activation distribution")
+plt.show()
